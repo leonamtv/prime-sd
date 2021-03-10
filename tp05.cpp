22ae3d72eee6192205c5584a67a8c478f60c1b0a
Leonam <leonan.aiirrc02t@gmail.com>
2021-03-10 10:06:46 -0300

    feat: add command line arguments to software

--- tp05.cpp
+++ tp05.cpp
@@ -2,13 +2,11 @@
 #include <iostream>
 #include <chrono>
 
-#define NUM_THREADS 4
-#define TEMPO_EXEC_SEG 40
-
 unsigned long long int control = 2;
+int num_threads = 4, tempo_exec_sec = 40;
 
 void * verifica_primo ( void * args  );
-int main ( );
+int main ( int argc, char* argv[] );
 
 pthread_mutex_t mutex_a;
 pthread_mutex_t mutex_b;
@@ -37,20 +35,33 @@ void * verifica_primo ( void * args ) {
     return 0;
 }
 
-int main () {
-    pthread_t* id_control_threads = ( pthread_t * ) malloc ( sizeof ( pthread_t* ) * NUM_THREADS );
+int main ( int argc, char* argv[] ) {
+    
+    switch ( argc ) {
+        case 1:
+            break;
+        case 2:
+            num_threads = std::atoi ( argv[1] );
+            break;
+        case 3:
+            num_threads = std::atoi ( argv[1] );
+            tempo_exec_sec = std::atoi ( argv[2] );
+            break;
+    }
+
+    pthread_t* id_control_threads = ( pthread_t * ) malloc ( sizeof ( pthread_t* ) * num_threads );
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
-    for ( int i = 0; i < NUM_THREADS; i++ ) {
+    for ( int i = 0; i < num_threads; i++ ) {
         if ( pthread_create ( &id_control_threads[i], NULL, verifica_primo, NULL ))
             puts("Erro na criacao de threads.");
     }
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    while ( std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= TEMPO_EXEC_SEG ) {
+    while ( std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= tempo_exec_sec ) {
         end = std::chrono::steady_clock::now();
     }
-    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{TEMPO_EXEC_SEG} )
+    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{tempo_exec_sec} )
         return 0;
-    for ( int i = 0; i < NUM_THREADS; i++ ) {
+    for ( int i = 0; i < num_threads; i++ ) {
         if ( pthread_join ( id_control_threads[i], NULL )) {
             puts ( "Erro de sincronizacao das threads. " );
             return -1;
--- tp05_s.cpp
+++ tp05_s.cpp
@@ -1,9 +1,8 @@
 #include <iostream>
 #include <chrono>
 
-#define TEMPO_EXEC_SEG 40
-
 unsigned long long int control = 0;
+int tempo_exec_sec = 40;
 
 void verifica_primo ();
 int main ();
@@ -20,14 +19,23 @@ void verifica_primo ( ) {
         std::cout << aux << std::endl;
 }
 
-int main () {
+int main (int argc, char* argv[] ) {
+    
+    switch ( argc ) {
+        case 1:
+            break;
+        case 2:
+            tempo_exec_sec = std::atoi ( argv[1] );
+            break;
+    }
+
     std::chrono::steady_clock::time_point begin = std::chrono::steady_clock::now();
     std::chrono::steady_clock::time_point end = std::chrono::steady_clock::now();
-    while ( std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= TEMPO_EXEC_SEG ) {
+    while ( std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= tempo_exec_sec ) {
         verifica_primo();
         end = std::chrono::steady_clock::now();
     }
-    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{TEMPO_EXEC_SEG} )
+    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{tempo_exec_sec} )
         return 0;
     return 0;
 }
