3cd469774246ed597a6de52968946f0eb1768f5c
Leonam <leonan.aiirrc02t@gmail.com>
2021-03-10 17:11:29 -0300

    fix: ignore non existing files to remove

--- cp.sh
+++ cp.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Compilando tp05.cpp..."
-rm bin/tp.out
+rm -f bin/tp.out
 g++ tp05.cpp -l pthread -o bin/tp.out
--- cs.sh
+++ cs.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 echo "Compilando tp05_s.cpp..."
-rm bin/tps.out
+rm -f bin/tps.out
 g++ tp05_s.cpp -o bin/tps.out
