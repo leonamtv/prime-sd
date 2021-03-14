7c6b55f21ed5d858fb6eb154fa5ad273364d4c2a
Leonam <leonan.aiirrc02t@gmail.com>
2021-03-13 21:33:46 -0300

    feat: adding square root approach

--- tp05.cpp
+++ tp05.cpp
@@ -1,8 +1,10 @@
 #include <pthread.h>
 #include <iostream>
 #include <chrono>
+#include <cmath>
 
 unsigned long long int control = 2;
+unsigned long long number_of_primes = 0;
 int num_threads = 4, tempo_exec_sec = 40;
 
 void * verifica_primo ( void * args  );
@@ -17,7 +19,7 @@ void * verifica_primo ( void * args ) {
         pthread_mutex_lock( &mutex_b );
         control ++;
         pthread_mutex_unlock( &mutex_b );
-        int div = aux - 1;
+        int div = (int) sqrt( aux );
         int divisors = 0;
         while ( div > 1 ) {
             if ( aux % div == 0 && div > 1 ) 
@@ -26,7 +28,7 @@ void * verifica_primo ( void * args ) {
         }
         if ( divisors == 0 ) {
             pthread_mutex_lock( &mutex_a );
-            std::cout << aux << std::endl;
+            number_of_primes ++;
             pthread_mutex_unlock( &mutex_a );
         }
     }
@@ -59,8 +61,10 @@ int main ( int argc, char* argv[] ) {
     while ( std::chrono::duration_cast<std::chrono::seconds>(end - begin).count() <= tempo_exec_sec ) {
         end = std::chrono::steady_clock::now();
     }
-    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{tempo_exec_sec} )
+    if ( std::chrono::duration_cast<std::chrono::seconds>(end - begin) >= std::chrono::seconds{tempo_exec_sec} ) {
+        std::cout << number_of_primes << std::endl;
         return 0;
+    }
     for ( int i = 0; i < num_threads; i++ ) {
         if ( pthread_join ( id_control_threads[i], NULL )) {
             puts ( "Erro de sincronizacao das threads. " );
